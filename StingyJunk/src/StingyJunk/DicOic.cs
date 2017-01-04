0e8f3b60ae043571f1ad374bde2c96ca0b8add28
Andrew Stanton <astanton1978@hotmail.com>
2017-01-03 22:20:41 -0500

    Trying out netcore again, hope no disappoint

---
+++ StingyJunk/src/StingyJunk/DicOic.cs
@@ -0,0 +1,16 @@
+﻿namespace StingyJunk
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     A case insensitive string keyed dictionary
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DicOic<T> : Dictionary<string, T>
+    {
+        public DicOic() : base(StringComparer.OrdinalIgnoreCase)
+        {
+        }
+    }
+}

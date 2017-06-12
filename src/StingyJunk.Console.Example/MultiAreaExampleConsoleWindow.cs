bd61b3fe3a6084754d4d63dcd1ef1fc0d98ec552
Andrew Stanton <astanton1978@hotmail.com>
2017-06-11 22:07:36 -0400

    more flair

--- src/StingyJunk.Console.Example/MultiAreaExampleConsoleWindow.cs
+++ src/StingyJunk.Console.Example/MultiAreaExampleConsoleWindow.cs
@@ -1,17 +1,20 @@
 ﻿namespace StingyJunk.Console.Example
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
+    using StingyJunk.Extensions;
 
     internal static class MultiAreaExampleConsoleWindow
     {
         private static ConsoleWindow _consoleWindow;
-        
+        private static LinkedList<Flair> _flairs;
+        private static LinkedListNode<Flair> _lastFlair;
         private static void Main()
         {
             Thread.CurrentThread.Name = nameof(SimpleExampleConsoleWindow);
-
+            _flairs = BuildFlairLibrary();
 
             var header = new DisplayArea("Header", 0, 0, 5, Console.WindowWidth);
 
@@ -20,7 +23,7 @@
             scrollingMessages.Cycle = true;
 
 
-            _consoleWindow = new ConsoleWindow(new[] {header, scrollingMessages});
+            _consoleWindow = new ConsoleWindow(new[] { header, scrollingMessages });
             WriteHeader($"This is the head");
 
             Task.Run(() =>
@@ -45,10 +48,48 @@
         }
 
 
-        private static void WriteMessage(string msg, bool isErr = false)
+        private static void WriteMessage(string msg)
         {
-            var flair = isErr ? Flair.Error : Flair.Log;
-            _consoleWindow.WriteLine(msg, flair, "Messages");
+            LinkedListNode<Flair> flair = null;
+            if (_lastFlair == null)
+            {
+                _lastFlair = _flairs.First;
+                flair = _lastFlair;
+            }
+            else
+            {
+                flair = _lastFlair.NextOrFirst();
+                _lastFlair = flair;
+            }
+            
+
+            _consoleWindow.WriteLine(msg, flair.Value, "Messages");
         }
+
+        private static LinkedList<Flair> BuildFlairLibrary()
+        {
+            var flairs = new LinkedList<Flair>();
+            foreach (ConsoleColor foregroundColor in Enum.GetValues(typeof(ConsoleColor)))
+            {   
+                var backgroundColor = ConsoleColor.Black;
+                if ((int) foregroundColor < 10)
+                {
+                    backgroundColor = ConsoleColor.White;
+                }
+                
+                var flair = new Flair(foregroundColor, backgroundColor);
+                if (flairs.Count == 0)
+                {
+                    flairs.AddFirst(flair);
+                }
+                else
+                {
+                    flairs.AddAfter(flairs.Last, flair);        
+                }
+            }
+            
+            return flairs;
+        }
+
     }
 }
---
+++ src/StingyJunk/Extensions/Lists.cs
@@ -0,0 +1,29 @@
+﻿namespace StingyJunk.Extensions
+{
+    using System.Collections.Generic;
+
+    public static class Lists
+    {
+        /// <summary>
+        ///     Cheap circular linked list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="currentNode"></param>
+        /// <returns></returns>
+        public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> currentNode)
+        {
+            return currentNode.Next ?? currentNode.List.First;
+        }
+
+        /// <summary>
+        ///     Cheap circular linked list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="currentNode"></param>
+        /// <returns></returns>
+        public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> currentNode)
+        {
+            return currentNode.Previous ?? currentNode.List.Last;
+        }
+    }
+}
